81cb6e186bd3acc7db048327dc9329f5ab4de2b1
Sergei Mekhavich <serak_96@mail.ru>
2024-12-19 23:19:40 +0300

    refactoring

--- src/domain/users-service.ts
+++ src/domain/users-service.ts
@@ -2,48 +2,28 @@ import {usersDBType} from "../db/dbType";
 import {userInputType} from "../types/usersType";
 import bcrypt from "bcrypt"
 import {ObjectId} from "mongodb";
-import {usersRepository} from "../repositories/users-repository";
+import {UsersRepository, usersRepository} from "../repositories/users-repository";
 import {AccountData, AuthDBTypeClass, EmailConfirmation, PasswordRecovery} from "../types/authType";
-import {v4 as uuidv4} from "uuid/dist/esm";
+import {v4 as uuidv4} from "uuid";
 import {add} from "date-fns";
 
 
-export const usersService = {
-
+class UsersService {
     async createdUser(userCreatedData: userInputType): Promise<string> {
 
         const passwordSalt = await bcrypt.genSalt(10)
         const passwordHash = await this._generateHash(userCreatedData.password, passwordSalt)
 
 
-        /*const newUser: usersDBType = {
-            _id: new ObjectId(),
-            accountData: {
-                createdAt: new Date().toISOString(),
-                login: userCreatedData.login,
-                email: userCreatedData.email,
-                passwordHash,
-                passwordSalt
-            },
-            emailConfirmation: {
-                confirmationCode: null,
-                expirationData: null,
-                isConfirmed: true
-            },
-            passwordRecovery: {
-                expirationDate: null,
-                recoveryCode: null
-            }
-        }*/
-
-        const newUser: usersDBType = new AuthDBTypeClass(new ObjectId(),
-            new AccountData(new Date().toISOString(), userCreatedData.login, userCreatedData.email, passwordHash, passwordSalt),
-            new EmailConfirmation(null, null, true),
-            new PasswordRecovery(null, null)
+        const newUser: usersDBType = new AuthDBTypeClass( new ObjectId(),
+            new AccountData(new Date().toISOString(),userCreatedData.login,userCreatedData.email,passwordHash,passwordSalt),
+            new EmailConfirmation(null,null,true),
+            new PasswordRecovery(null,null)
         )
 
         return usersRepository.createdUser(newUser)
-    },
+    }
+
     async checkCredentials(loginOrEmail: string, password: string): Promise<usersDBType | null> {
         //// проверить на подтвержденный код
         const user = await usersRepository.findByLoginOrEmail(loginOrEmail)
@@ -53,15 +33,19 @@ export const usersService = {
             return null
         }
         return user
-    },
+    }
+
     async deleteUser(id: string): Promise<boolean> {
         return await usersRepository.deleteUser(id)
-    },
+    }
+
     async findUserById(id: ObjectId): Promise<null | usersDBType> {
         return await usersRepository.findUserById(id)
-    },
+    }
+
     async _generateHash(password: string, salt: string) {
         return await bcrypt.hash(password, salt)
     }
-
 }
+
+export const usersService = new UsersService()
--- src/repositories/users-repository.ts
+++ src/repositories/users-repository.ts
@@ -3,32 +3,31 @@ import {ObjectId, WithId} from "mongodb";
 import {usersMongooseModel} from "../db/mongooseSchema/mongooseSchema";
 
 
-export const usersRepository = {
-
+export class UsersRepository{
     async createdUser(newUserData: usersDBType): Promise<string> {
 
         const newUser = await usersMongooseModel.create(newUserData)
         return newUser._id.toString()
 
-    },
+    }
     async findUserByConfirmCode(code: string): Promise<usersDBType | null> {
         ///return await usersCollection.findOne({'emailConfirmation.confirmationCode': code})
         return usersMongooseModel.findOne({'emailConfirmation.confirmationCode': code})
-    },
+    }
     async updateConfirmationCode(_id: ObjectId, code: string): Promise<boolean> {
         let result = await usersMongooseModel.updateOne({_id}, {$set: {'emailConfirmation.confirmationCode': code}})
         return result.matchedCount === 1
-    },
+    }
     async findUserById(id: ObjectId): Promise<usersDBType | null> {
         const user = await usersMongooseModel.findOne({_id: id})
         if (!user) return null
         return user
-    },
+    }
     async findByLoginOrEmail(loginOrEmail: string): Promise<WithId<usersDBType> | null> {
         return usersMongooseModel.findOne({
             $or: [{'accountData.email': loginOrEmail}, {'accountData.login': loginOrEmail}],
         });
-    },
+    }
     async deleteUser(id: string): Promise<boolean> {
         if (!this._checkObjectId(id)) return false;
         const userInstance = await usersMongooseModel.findOne({_id: new ObjectId(id)})
@@ -36,11 +35,11 @@ export const usersRepository = {
         await userInstance.deleteOne()
         return true
 
-    },
+    }
     async updateConfirmationStatus(_id: ObjectId): Promise<boolean> {
         let result = await usersMongooseModel.updateOne({_id}, {$set: {'emailConfirmation.isConfirmed': true}})
         return result.matchedCount === 1
-    },
+    }
     async updatePasswordRecoveryData(_id: ObjectId, expirationDate: Date, recoveryCode: string): Promise<boolean> {
         const userInstance = await usersMongooseModel.findOne({_id})
         if (!userInstance) return false
@@ -48,10 +47,10 @@ export const usersRepository = {
         userInstance.passwordRecovery.expirationDate = expirationDate
         await userInstance.save()
         return true
-    },
+    }
     async findUserByPasswordRecoveryCode(recoveryCode: string): Promise<usersDBType | null> {
         return usersMongooseModel.findOne({'passwordRecovery.recoveryCode':recoveryCode})
-    },
+    }
     async updatePassword(_id: ObjectId, passwordSold: string, passwordHash: string): Promise<boolean> {
         const userInstance = await usersMongooseModel.findOne({_id})
         if (!userInstance) return false
@@ -61,8 +60,10 @@ export const usersRepository = {
         userInstance.passwordRecovery.recoveryCode = null
         userInstance.save()
         return true
-    },
+    }
     _checkObjectId(id: string): boolean {
         return ObjectId.isValid(id)
     }
 }
+
+export const usersRepository = new UsersRepository()
